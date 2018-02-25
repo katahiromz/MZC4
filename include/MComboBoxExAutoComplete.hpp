e7d7286bd138bd25f1b01605155fbf5a24764b7a
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-02-25 12:36:37 +0900

    add MComboBoxExAutoComplete

---
+++ include/MComboBoxExAutoComplete.hpp
@@ -0,0 +1,124 @@
+// MComboBoxExAutoComplete.hpp -- autocomplete combobox extended -*- C++ -*-
+// This file is part of MZC4.  See file "ReadMe.txt" and "License.txt".
+////////////////////////////////////////////////////////////////////////////
+
+#ifndef MZC4_MCOMBOBOXEXAUTOCOMP_HPP_
+#define MZC4_MCOMBOBOXEXAUTOCOMP_HPP_     0   /* Version 0 */
+
+class MComboBoxExAutoComplete;
+
+#include "MEditCtrl.hpp"
+#include "MComboBoxEx.hpp"
+
+////////////////////////////////////////////////////////////////////////////
+
+class MComboBoxExEditAutoComplete : public MEditCtrl
+{
+public:
+    MComboBoxExEditAutoComplete() : m_bAutoComplete(FALSE)
+    {
+    }
+
+    void OnKey(HWND hwnd, UINT vk, BOOL fDown, int cRepeat, UINT flags)
+    {
+        if (vk == VK_DELETE || vk == VK_BACK)
+            m_bAutoComplete = FALSE;
+        else
+            m_bAutoComplete = TRUE;
+
+        if (fDown)
+        {
+            FORWARD_WM_KEYDOWN(hwnd, vk, cRepeat, flags, DefaultProcDx);
+        }
+        else
+        {
+            FORWARD_WM_KEYUP(hwnd, vk, cRepeat, flags, DefaultProcDx);
+        }
+    }
+
+    virtual LRESULT CALLBACK
+    WindowProcDx(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+    {
+        switch (uMsg)
+        {
+            HANDLE_MSG(hwnd, WM_KEYDOWN, OnKey);
+            HANDLE_MSG(hwnd, WM_KEYUP, OnKey);
+        }
+        return DefaultProcDx();
+    }
+
+public:
+    BOOL m_bAutoComplete;
+};
+
+class MComboBoxExAutoComplete : public MComboBoxEx
+{
+public:
+    MComboBoxExAutoComplete()
+    {
+    }
+
+    virtual void PostSubclassDx(HWND hwnd)
+    {
+        HWND hEdit = FindWindowEx(hwnd, NULL, TEXT("EDIT"), NULL);
+        m_edit.SubclassDx(hEdit);
+    }
+
+    void OnEditChange()
+    {
+        DWORD dwPos;
+        OnEditChange(dwPos);
+    }
+
+    void OnEditChange(DWORD& dwPos)
+    {
+        if (!m_edit.m_bAutoComplete)
+        {
+            return;
+        }
+
+        MString strInput = GetWindowText();
+
+        dwPos = GetEditSel();
+        MString strRight = strInput.substr(HIWORD(dwPos));
+        mstr_trim(strRight);
+        if (!strRight.empty())
+            return;
+
+        mstr_trim(strInput);
+        MString strInputUpper = strInput;
+        _wcsupr(&strInputUpper[0]);
+
+        INT iItem = FindString(-1, strInput.c_str());
+        if (iItem == CB_ERR)
+            return;
+
+        TCHAR szText[128];
+        GetLBText(iItem, szText);
+        MString strCandidate = szText;
+
+        INT nCount = GetCount();
+        for (INT i = iItem + 1; i < nCount; ++i)
+        {
+            GetLBText(i, szText);
+            MString strText = szText;
+            _wcsupr(&strText[0]);
+            if (strText.find(strInputUpper) == 0)
+            {
+                return;
+            }
+        }
+
+        m_edit.m_bAutoComplete = FALSE;
+        SetWindowText(strCandidate.c_str());
+        SetEditSel(INT(strInput.size()), INT(strCandidate.size()));
+        dwPos = MAKELONG(INT(strInput.size()), INT(strCandidate.size()));
+    }
+
+public:
+    MComboBoxExEditAutoComplete m_edit;
+};
+
+////////////////////////////////////////////////////////////////////////////
+
+#endif  // ndef MZC4_MCOMBOBOXEXAUTOCOMP_HPP_
